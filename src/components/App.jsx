4eed81e8c914a51ae46fe6e7a13fb1b8738a2906
LiudmylaShevchuk <lashevchuk12@gmail.com>
2023-04-10 13:17:41 +0300

    App.jsx changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
 
-import user from '../user.json';
+import user from '../data/user';
 import data from 'data.json';
 import friends from 'friends.json';
 import transactions from 'transactions.json';
